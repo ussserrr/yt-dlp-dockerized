7dbb48a9c0c76a311c38f8c81bfc25c146bb0bbb
ussserrr <andrei4.2008@gmail.com>
2026-02-19 01:03:54 +0300

    Use "docker run --user" option

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
-export DOCKER_BUILDKIT=1
-export CONTAINER_USER=$(whoami)
-export USER_ID=$(id -u $CONTAINER_USER)
-export GROUP_ID=$(id -g $CONTAINER_USER)
+: "${CONTAINER_USER:=$(whoami)}"
+: "${USER_ID:=$(id -u $CONTAINER_USER)}"
+: "${GROUP_ID:=$(id -g $CONTAINER_USER)}"
 
-docker build \
-	--build-arg HTTP_PROXY \
-	--build-arg CONTAINER_USER=$CONTAINER_USER \
-	--build-arg USER_ID=$USER_ID \
-	--build-arg GROUP_ID=$GROUP_ID \
+docker buildx build \
+    --build-arg HTTP_PROXY \
+    --build-arg CONTAINER_USER=$CONTAINER_USER \
+    --build-arg USER_ID=$USER_ID \
+    --build-arg GROUP_ID=$GROUP_ID \
     --tag yt-dlp \
     .
